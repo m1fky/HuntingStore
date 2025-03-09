c03438b1278e7dd55b6e39de4f6fd0dafa99b615
Zorigto Radnaev <zorigto.rd37122@gmail.com>
2025-03-09 21:16:38 +0700

    added 4 tables to init.sql

--- initDB/data/init.sql
+++ initDB/data/init.sql
@@ -5,9 +5,35 @@ CREATE TABLE IF NOT EXISTS users (
                                      name TEXT NOT NULL,
                                      phone TEXT,
                                      address TEXT,
-                                     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+                                     created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+CREATE TABLE IF NOT EXISTS products (
+                                     id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                     name TEXT NOT NULL,
+                                     description TEXT,
+                                     price FLOAT NOT NULL,
+                                     stock INT NOT NULL DEFAULT 0,
+                                     category TEXT,
+                                     created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+CREATE TABLE IF NOT EXISTS orders (
+                                     id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                     user_id INTEGER FOREIGN KEY REFERENCES users(id),
+                                     status TEXT DEFAULT "new",
+                                     total_price FLOAT NOT NULL,
+                                     created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 CREATE TABLE IF NOT EXISTS roles (
-                       id INTEGER PRIMARY KEY AUTOINCREMENT,
-                       name TEXT UNIQUE NOT NULL
+                                     id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                     name TEXT UNIQUE NOT NULL
+);
+CREATE TABLE IF NOT EXISTS order_items (
+                                     order_id INTEGER FOREIGN KEY REFERENCES orders(id),
+                                     product_id INTEGER FOREIGN KEY REFERENCES products(id),
+                                     quantity INTEGER NOT NULL
+);
+CREATE TABLE IF NOT EXISTS user_roles (
+                                     user_id INTEGER FOREIGN KEY REFERENCES users(id),
+                                     role_id INTEGER FOREIGN KEY REFERENCES roles(id),
+                                     quantity INTEGER NOT NULL
 );
