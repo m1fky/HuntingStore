06ce080238d45e764487077bd8967ff5bf90ea35
zorigto <zorigto.radnaev04@mail.ru>
2025-03-20 22:00:14 +0700

    перенастройка проекта

--- backend/HuntingStore/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class HuntingstoreConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'HuntingStore'
--- backend/shop/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import User, Role, UserRole, Product, Order, OrderItem
-
-admin.site.register(User)
-admin.site.register(Role)
-admin.site.register(UserRole)
-admin.site.register(Product)
-admin.site.register(Order)
-admin.site.register(OrderItem)
